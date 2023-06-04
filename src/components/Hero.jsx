3ba1267ecfb5469ec9a2182493f6b483505a6f43
shankarkancha <shankarkancha45@gmail.com>
2023-06-04 16:22:26 +0530

    url edited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="scumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open('https://github.com/Shankarkancha')}
+          onClick={() => window.open('https://github.com/Shankarkancha/AI_SUMMARIZER')}
           className="black_btn"
         >GitHub</button>
       </nav>
